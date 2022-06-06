76e6982a799d2372c7ce4280048c52f035a8b7ac
Lorenzo Busoni <lbusoni@gmail.com>
2022-06-06 14:26:50 +0200

    modified to allow installation on 4sight1.8 or 4sight2.23

--- setup.py
+++ setup.py
@@ -58,8 +58,8 @@ class UploadCommand(Command):
         sys.exit()
 
 
-class InstallWykoCommand(Command):
-    """Support setup.py install_wyko"""
+class Install4Sight223Command(Command):
+    """Support setup.py install_4sight223"""
 
     description = 'Install files needed to communicate with Wyko4100 4sight2.23 via Pyro'
     user_options = []
@@ -100,7 +100,62 @@ class InstallWykoCommand(Command):
         self.status(
             'Installing pyro server startup script in %s' % script_dest)
         self.copy_file(os.path.join('archive_for_wyko',
-                                    'ServerStartup.py'), script_dest)
+                                    'ServerStartup223.py'), script_dest)
+
+        self.status('Installing Pyro.conf in %s' % root_dest)
+        self.copy_file(os.path.join(
+            'archive_for_wyko', 'Pyro.conf'), root_dest)
+
+        pyro_dir = os.path.join('C:', os.sep, '4D', 'Pyro')
+        self.status('Creating folder %s' % pyro_dir)
+        self.mkpath(pyro_dir)
+
+        sys.exit()
+
+
+class Install4Sight18Command(Command):
+    """Support setup.py install_4sight18"""
+
+    description = 'Install files needed to communicate with Phasecam4020 4sight1.8 via Pyro'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        print(s)
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self.status(self.description)
+        root_dest = os.path.join('C:', os.sep, 'Program Files', '4sight1.8')
+        print('4sight root folder is: %s' % root_dest)
+        self.status('Installing pyro in current python environment')
+        os.chdir(os.path.join('archive_for_wyko', 'Pyro-3.6'))
+        os.system('python setup.py install')
+        os.chdir(here)
+
+        site_package_dest = os.path.join(root_dest, 'scripts', 'site-packages')
+        self.status(
+            'Installing 3rd part modules in %s' % site_package_dest)
+        self.copy_tree(
+            os.path.join('archive_for_wyko', 'Pyro-3.6', 'Pyro'),
+            os.path.join(site_package_dest, 'Pyro'))
+        self.copy_tree(
+            os.path.join('archive_for_wyko', 'site-packages'),
+            os.path.join(site_package_dest))
+        self.copy_tree(
+            os.path.join('plico_interferometer_server', 'i4sight18'),
+            os.path.join(site_package_dest, 'i4sight18'))
+
+        script_dest = os.path.join(root_dest, 'scripts')
+        self.status(
+            'Installing pyro server startup script in %s' % script_dest)
+        self.copy_file(os.path.join('archive_for_wyko',
+                                    'ServerStartup18.py'), script_dest)
 
         self.status('Installing Pyro.conf in %s' % root_dest)
         self.copy_file(os.path.join(
@@ -154,5 +209,7 @@ setup(name=NAME,
                         ],
       include_package_data=True,
       test_suite='test',
-      cmdclass={'upload': UploadCommand, 'install_wyko': InstallWykoCommand, },
+      cmdclass={'upload': UploadCommand,
+                'install_4sight223': Install4Sight223Command,
+                'install_4sight18': Install4Sight18Command, },
       )
